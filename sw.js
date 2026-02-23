98e4dafd6dbf686b7203650a052b3321e45526de
danimp90mysql-cpu <dani.mp90.mysql@gmail.com>
2026-02-23 14:22:29 +0100

    Segunda versión FIRE APP

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fire-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'fire-v2';
+const ASSETS = ['./index.html', './manifest.json', './sw.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
